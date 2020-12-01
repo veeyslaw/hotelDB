376b06e5ae70bcf1caaf05b3d11af325207e8624
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-12-01 12:23:41 +0200

    Create initial ui

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        loadUi("Front/form.ui", self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
