3eedab1f0edbfb9ddead5e20f8e1c2e506344a91
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-12-03 00:37:57 +0200

    Create guest and city populate scripts

---
+++ Utility/populate.sql
@@ -0,0 +1,12 @@
+INSERT INTO GUEST (first_name, last_name, passport_number, email, phone_number) VALUES ('Iuliana', 'Berdianu', 'RO252342', 'iuliana.berdianu@gmail.com', '+40764974854');
+INSERT INTO GUEST (first_name, last_name, passport_number, email, phone_number) VALUES ('Dumitru', 'Frunza', 'RO525355', 'dumitrufrunza@gmail.com', '+40744652474');
+INSERT INTO GUEST (first_name, last_name, passport_number, email) VALUES ('Ion', 'Ceban', 'MDC52522', 'cebanion84@mail.ru');
+INSERT INTO GUEST (first_name, last_name, passport_number, email, phone_number) VALUES ('Adrian', 'Smith', 'UK2432L52342', 'asmith@yahoo.com', '+443526284958');
+INSERT INTO GUEST (first_name, last_name, passport_number, email) VALUES ('Alexandrin', 'Dogot', 'IT251NA321542', 'a1dogot@gmail.com');
+
+INSERT INTO CITY (city_name) VALUES ('Iasi');
+INSERT INTO CITY (city_name) VALUES ('Bucuresti');
+INSERT INTO CITY (city_name) VALUES ('Cluj');
+INSERT INTO CITY (city_name) VALUES ('Constanta');
+INSERT INTO CITY (city_name) VALUES ('Sinaia');
+
