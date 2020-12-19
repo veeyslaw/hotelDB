bc85c185ecd718f05e26d6087089332397ea80eb
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-12-19 16:55:15 +0200

    Rename number to count. Change apartment primary key

--- Utility/populate.sql
+++ Utility/populate.sql
@@ -1,15 +1,18 @@
+-- GUESTS
 INSERT INTO GUEST (first_name, last_name, passport_number, email, phone_number) VALUES ('Iuliana', 'Berdianu', 'RO252342', 'iuliana.berdianu@gmail.com', '+40764974854');
 INSERT INTO GUEST (first_name, last_name, passport_number, email, phone_number) VALUES ('Dumitru', 'Frunza', 'RO525355', 'dumitrufrunza@gmail.com', '+40744652474');
 INSERT INTO GUEST (first_name, last_name, passport_number, email) VALUES ('Ion', 'Ceban', 'MDC52522', 'cebanion84@mail.ru');
 INSERT INTO GUEST (first_name, last_name, passport_number, email, phone_number) VALUES ('Adrian', 'Smith', 'UK2432L52342', 'asmith@yahoo.com', '+443526284958');
 INSERT INTO GUEST (first_name, last_name, passport_number, email) VALUES ('Alexandrin', 'Dogot', 'IT251NA321542', 'a1dogot@gmail.com');
 
+-- CITIES
 INSERT INTO CITY (city_name) VALUES ('Iasi');
 INSERT INTO CITY (city_name) VALUES ('Bucuresti');
 INSERT INTO CITY (city_name) VALUES ('Cluj');
 INSERT INTO CITY (city_name) VALUES ('Constanta');
 INSERT INTO CITY (city_name) VALUES ('Sinaia');
 
+-- HOTELS AND DESCRIPTIONS
 INSERT INTO HOTEL (hotel_name, city_id, contact_number, manager_name)
 VALUES ('Stefan cel Mare Hotel', (SELECT city_id FROM CITY WHERE city_name = 'Iasi'), '+40789235342', 'Ana-Maria Iftime');
 INSERT INTO HOTEL_DESCRIPTION (hotel_id, text_desc, rating, restaurant, free_meals, pool, free_internet)
@@ -28,12 +31,76 @@ VALUES (HOTEL_HOTEL_ID_SEQ.CURRVAL, 'Hotel prietenos cu mediul.', 2, 0, 0, 0, 0)
 
 
 INSERT INTO HOTEL (hotel_name, city_id, contact_number, manager_name)
-VALUES ('Universitatea Hotel', (SELECT city_id FROM CITY WHERE city_name = 'Constanta'), '+40753485432', 'Ana Rusu');
+VALUES ('Constanta Hotel', (SELECT city_id FROM CITY WHERE city_name = 'Constanta'), '+40753485432', 'Ana Rusu');
 INSERT INTO HOTEL_DESCRIPTION (hotel_id, text_desc, rating, restaurant, free_meals, pool, free_internet)
 VALUES (HOTEL_HOTEL_ID_SEQ.CURRVAL, 'Vacanta pe malul marii.', 4, 1, 1, 1, 1);
 
 
 INSERT INTO HOTEL (hotel_name, city_id, contact_number, manager_name)
-VALUES ('Universitatea Hotel', (SELECT city_id FROM CITY WHERE city_name = 'Sinaia'), '+40753499992');
+VALUES ('Peak Hotel', (SELECT city_id FROM CITY WHERE city_name = 'Sinaia'), '+40753499992');
 INSERT INTO HOTEL_DESCRIPTION (hotel_id, text_desc, rating, restaurant, free_meals, pool, free_internet)
 VALUES (HOTEL_HOTEL_ID_SEQ.CURRVAL, 'Hotelul se afla pe varful muntelui. Toate apartementele au o priveliste minunata.', 5, 1, 1, 1, 1);
+
+-- APARTMENTS AND DESCRIPTIONS
+-- stefan
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Stefan cel Mare Hotel'), 3, 6, 300);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 1, 1, 1);
+
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Stefan cel Mare Hotel'), 2, 2, 200);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 1, 1, 0);
+
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Stefan cel Mare Hotel'), 2, 3, 150);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 0, 1, 1);
+
+-- univer
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Universitatea Hotel'), 1, 2, 75);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 0, 0, 0);
+
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Universitatea Hotel'), 1, 2, 50);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 0, 0, 0, 0);
+
+-- city
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'City Hotel'), 2, 3, 125);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 0, 0, 0, 0);
+
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'City Hotel'), 1, 3, 100);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 0, 0, 0, 1);
+
+-- constan
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Constanta Hotel'), 2, 4, 200);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 1, 0, 1);
+
+-- peak
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Peak Hotel'), 1, 2, 150);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 1, 1, 1);
+
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Peak Hotel'), 2, 4, 225);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 1, 1, 1);
+
+INSERT INTO APARTMENT (hotel_id, room_number, apart_capacity, price_per_night_euro)
+VALUES ((SELECT hotel_id FROM HOTEL WHERE hotel_name = 'Peak Hotel'), 4, 8, 375);
+INSERT INTO APARTMENT_DESCRIPTION (apart_id, air_conditioner, minibar, tv, double_bed)
+VALUES (APART_APART_ID_SEQ.CURRVAL, 1, 1, 1, 0);
+
+-- BOOKINGS
+
