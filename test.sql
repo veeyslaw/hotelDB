07588922d660e0fd10c46a3df70360eb8f780510
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-12-19 13:29:02 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,9 +1,6 @@
-SAVEPOINT empty_tables;
-ROLLBACK TO SAVEPOINT empty_tables;
-
 INSERT INTO CITY (city_name) VALUES ('Iasi');
 INSERT INTO HOTEL (hotel_name, city_id, contact_number, manager_name)
-VALUES ('Iasi Hotel', SELECT city_id FROM CITY WHERE city_name = 'Iasi', '0789235342', 'Ana-Maria');
+VALUES ('Iasi Hotel', (SELECT city_id FROM CITY WHERE city_name = 'Iasi'), '+40789235342', 'AnaMaria');
 
 SELECT APART_APART_ID_SEQ.CURRVAL FROM DUAL;
 SELECT APART_APART_ID_SEQ.NEXTVAL FROM DUAL;
